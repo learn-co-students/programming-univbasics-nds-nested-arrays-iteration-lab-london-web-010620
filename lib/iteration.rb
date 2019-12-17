536c97ce58bdf6d39f24fcf2b27f0f88eb62ef54
Sergio Robles <sroblesc@gmail.com>
2019-12-17 23:16:40 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -57,26 +57,23 @@ end
 
 def total_even_pairs(src)
  array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
- row_index = 0 
- src = []
- total = 0
+row_index = 0 
+
   while row_index < array_3.count do 
-    if array_3[row_index][0] % 2 == 1 
-      src << array_3[row_index][0]
-    if array_3[row_index][0] % 2 == 0 
-      src << array_3[row_index][0] = 0 
-    if array_3[row_index][1] % 2 == 1 
-      src << array_3[row_index][1]
-    if array_3[row_index][1] % 2 == 0 
-      src << array_3[row_index][1] = 0 
-    end
-    row_index += 1
+    element_index = 0
+    src = []
+      while element_index < array_3.count do 
+        total = 0
+        if array_3[row_index][element_index] % 2 == 0 
+          src << array_3[row_index][element_index]
+        end 
+        total + src[row_index][element_index]
+        element_index += 1 
+      end 
+      row_index += 1
   end
-  element_index = 0
-   while element_index < src.count do 
-     total += src[row_index][element_index]
-    element_index +=1
-    end
   return total
-end 
+  end
+  
+  
   
